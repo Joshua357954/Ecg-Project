37ca26ed34b49cbf5b1a084ceb127e0df9a0573b
joshua boyi <boyijoshua72@gmail.com>
2024-05-18 18:07:37 +0100

    new

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,57 @@
 "use client";
 
 import React, { useState } from "react";
-import { Canvas, extend, useThree } from "@react-three/fiber";
+import { Canvas, extend } from "@react-three/fiber";
 import { Environment, Html, useGLTF } from "@react-three/drei";
-import * as THREE from "three";
 import { Toaster, toast } from "react-hot-toast";
+import * as THREE from "three";
 
 extend({ Html });
 
-const Marker = ({ position, color, onDrop }) => {
+const Marker = ({ position, onDrop }) => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [currentColor, setCurrentColor] = useState(position.defaultColor);
+
   const handleDragOver = (event) => {
     event.preventDefault();
+  };
+
+  const handleDragEnter = () => {
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
   };
 
   const handleDrop = (event) => {
     event.preventDefault();
     const droppedColor = event.dataTransfer.getData("color");
-    onDrop(color === droppedColor);
+    setCurrentColor(droppedColor);
+    onDrop(position.id, position.color === droppedColor);
+    setIsDragging(false);
+  };
+
+  const markerStyle = {
+    border: "2px dotteds black",
+    width: "1.3rem",
+    height: "1.3rem",
+    borderRadius: "50%",
+    cursor: "default",
+    backgroundColor: currentColor,
+    opacity: isDragging ? 0.5 : 1,
   };
 
   return (
-    <mesh className="cursor-pointer relative" position={position}>
+    <mesh position={position.coords}>
       <Html>
-        <boxGeometry args={[0.2, 0.2, 0.2]} />
-        <meshBasicMaterial color={color} />
         <div
           onDragOver={handleDragOver}
           onDrop={handleDrop}
-          // style={{ backgroundColor: "gray" }}
-          className={`border-2 border-dotted border-black absolute top-0 left-0 w-5 h-5 rounded-full`}
-        ></div>
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+          style={markerStyle}
+        />
       </Html>
     </mesh>
   );
@@ -42,7 +64,7 @@ const Swatch = ({ color }) => {
 
   return (
     <div
-      className="w-8 h-8 rounded-full m-2"
+      className="w-4 h-4 rounded-full m-2"
       draggable
       onDragStart={handleDragStart}
       style={{ backgroundColor: color }}
@@ -53,33 +75,46 @@ const Swatch = ({ color }) => {
 const Model = ({ src, rotate }) => {
   const { scene } = useGLTF(src);
 
-  const positions = [
-    { x: -0.45, y: 1.79, z: 0.44, color: "red" },
-    { x: -0.1, y: 2.01, z: 0.49, color: "blue" },
-    { x: 0.33, y: 1.92, z: 0.5, color: "green" },
-    { x: 0.43, y: 1.49, z: 0.34, color: "yellow" },
-    { x: 0.21, y: 1.24, z: 0.46, color: "orange" },
-    { x: 0.0, y: 1.45, z: 0.51, color: "purple" },
-    { x: -0.2, y: 1.01, z: 0.48, color: "cyan" },
-    { x: -0.43, y: 1.03, z: 0.34, color: "magenta" },
-  ];
+  const [positions, setPositions] = useState([
+    { id: 1, coords: [-0.45, 1.79, 0.44], color: "red", defaultColor: "" },
+    { id: 2, coords: [-0.1, 2.01, 0.49], color: "blue", defaultColor: "" },
+    { id: 3, coords: [0.33, 1.92, 0.5], color: "green", defaultColor: "" },
+    { id: 4, coords: [0.43, 1.49, 0.34], color: "yellow", defaultColor: "" },
+    { id: 5, coords: [0.21, 1.24, 0.46], color: "orange", defaultColor: "" },
+    { id: 6, coords: [0.0, 1.45, 0.51], color: "purple", defaultColor: "" },
+    { id: 7, coords: [-0.2, 1.01, 0.48], color: "cyan", defaultColor: "" },
+    { id: 8, coords: [-0.43, 1.03, 0.34], color: "magenta", defaultColor: "" },
+  ]);
+
+  const handleDrop = (id, isCorrect) => {
+    setPositions((prevPositions) =>
+      prevPositions.map((position) =>
+        position.id === id
+          ? {
+              ...position,
+              currentColor: isCorrect ? position.color : position.defaultColor,
+            }
+          : position
+      )
+    );
+
+    toast(isCorrect ? "Correct Position!" : "Incorrect Position!", {
+      icon: isCorrect ? "🎉" : "😢",
+    });
+  };
 
   return (
     <>
       {scene && (
-        <primitive object={scene} scale={[3, 3, 3]} position={[0, -3, 0]} rotation-y={rotate} />
-      )}
-      {positions.map((position, index) => (
-        <Marker
-          key={index}
-          position={[position.x, position.y, position.z]}
-          color={position.color}
-          onDrop={(isCorrect) =>
-            toast(isCorrect ? "Correct Position!" : "Incorrect Position!", {
-              icon: isCorrect  ? "🎉" : "😢",
-            })
-          }
+        <primitive
+          object={scene}
+          scale={[3, 3, 3]}
+          position={[0, -3, 0]}
+          rotation-y={rotate}
         />
+      )}
+      {positions.map((position) => (
+        <Marker key={position.id} position={position} onDrop={handleDrop} />
       ))}
     </>
   );
@@ -116,7 +151,7 @@ const ThreeScene = () => {
             <ambientLight intensity={0.5} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
             <Model src="/human.glb" rotate={rotate} />
-            <Environment preset='sunset' />
+            <Environment preset="sunset" />
           </Canvas>
         </div>
 
